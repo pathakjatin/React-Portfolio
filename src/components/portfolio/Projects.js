62356770e238efd2914517e90d42edbb6ea33f32
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-11-06 21:04:18 +0530

    update projects

--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -1,18 +1,19 @@
 import todoImg from '../../assets/Screenshot 2024-08-03 222919.png';
 import signLangImg from '../../assets/Screenshot 2024-08-03 235904.png'
-import petConnImg from '../../assets/Screenshot 2024-08-03 231744.png'
-import investCalImg from '../../assets/Screenshot 2024-08-03 233757.png';
-import taskSphereImg from '../../assets/Screenshot 2024-08-03 234254.png';
-import spotifyImg from '../../assets/Screenshot 2024-08-04 020241.png';
+// import petConnImg from '../../assets/Screenshot 2024-08-03 231744.png'
+// import investCalImg from '../../assets/Screenshot 2024-08-03 233757.png';
+// import taskSphereImg from '../../assets/Screenshot 2024-08-03 234254.png';
+// import spotifyImg from '../../assets/Screenshot 2024-08-04 020241.png';
 import timeOutImg from '../../assets/Screenshot 2024-09-14 000202.png'
+import creditCardImg from '../../assets/Screenshot 2024-11-06 204724.png';
 
 export const project_data = [
     {
-        image : todoImg,
-        title : "ToDo List",
-        description : "A todo list app made using HTML, CSS & Javascript which uses local storage of the browser",
-        githublink : "https://github.com/pathakjatin/todo-list",
-        projectlink : "https://pathakjatin.github.io/todo-list/#",
+        image : creditCardImg,
+        title : "Credit Card Fraud Detection",
+        description : "A machine learning model that detects fraudulent credit card transactions by analyzing transaction patterns.",
+        githublink : "#",
+        projectlink : "#",
     },
     {
         image : signLangImg,
@@ -22,39 +23,17 @@ export const project_data = [
         projectlink : "https://github.com/pathakjatin/Sign-Language-Recognition-System",
     },
     {
-        image : petConnImg,
-        title : "PetConnect",
-        description : "A responsive website that allows users to find a diet for their beloved pets.",
-        githublink : "https://github.com/pathakjatin/PetConnect",
-        projectlink : "https://github.com/pathakjatin/PetConnect",
-    },
-    {
-        image : investCalImg,
-        title : "Investment Calculator",
-        description : "A React app that calculates investment made over years",
-        githublink : "https://github.com/pathakjatin/Investment-Calculator",
-        projectlink : "https://calculateinvest.netlify.app/",
-    },
-    {
         image : timeOutImg,
         title : "Time Out",
         description : "A fun game made using React to check one's intuitive skills",
         githublink : "https://github.com/pathakjatin/Time-Out",
         projectlink : "https://timerunsout.netlify.app/",
     },
-    
     {
-        image : taskSphereImg,
-        title : "TaskSphere",
-        description :"A MERN stack task management app." ,
-        githublink : "https://github.com/pathakjatin/",
-        projectlink : "#",
-    },
-    {
-        image : spotifyImg,
-        title : "Spotify Clone",
-        description : "An ad-free spotify clone website featuring my friend's favourite top songs ",
-        githublink : "https://github.com/pathakjatin/spotifyClone",
-        projectlink : "https://github.com/pathakjatin/spotifyClone",
+        image : todoImg,
+        title : "ToDo List",
+        description : "A todo list app made using HTML, CSS & Javascript which uses local storage of the browser",
+        githublink : "https://github.com/pathakjatin/todo-list",
+        projectlink : "https://pathakjatin.github.io/todo-list/#",
     },
 ];
