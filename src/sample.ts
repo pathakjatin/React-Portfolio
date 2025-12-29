e26bfa280f691e1d0a98fff316e2f3b9b98f2688
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2025-12-29 13:21:03 +0530

    new portfolio

--- src/sample.ts
+++ src/sample.ts
@@ -1,17 +1,17 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
+    name: `Jatin Pathak`,
+    nickname: `JP`,
+    linkedin: `www.linkedin.com/in/pathakjatin`,
+    ig: `https://www.instagram.com/pathakjatin_/`,
+    leetcode: `https://leetcode.com/u/pathakjatin/`,
+    email: 'mailto:workjatinpathak@gmail.com'
 
 }
 
 
 const buy = {
     title: `See on Github`,
-    link: `https://github.com/anthonylan/angie`
+    link: `https://github.com/pathakjatin`
 }
 
 
